d5726f6e0ef82110bfcac998089d6646ac3f4577
tobisey <tobi.seymour@gmail.com>
2020-03-25 12:18:17 +0000

    starting script

--- public/script.js
+++ public/script.js
@@ -6,9 +6,16 @@
     let input = document.getElementsByTagName("input")[0];
     let send = document.getElementById("send");
     var ta = document.getElementsByTagName('textarea')[0];
-    autosize(ta);
     var response = document.getElementById('response');
+    autosize(ta);
     var lastMessage = null;
+    var hue = 0;
+    var thinkingArr = ['.', '. .', '. . .'];
+    var thinkingCount = 0;
+    var thinkingInt;
+    var questions = {
+        creativity: ['How do you define creativity?', 'Should AI be used in the creative sphere?']
+    }
 
     setTimeout(() => {
         response.style.display = 'block';
@@ -17,12 +24,27 @@
     // functions
 
     function responseReceived(sentiment) {
-        console.log(sentiment);
+        // console.log(sentiment.sentimentScore);
+        if (sentiment.sentimentLabel === 'POSITIVE') {
+            hue += 20;
+            runway.style.filter = `hue-rotate(${hue}deg)`;
+        } else if (sentiment.sentimentLabel === 'NEGATIVE') {
+            hue -= 20;
+            runway.style.filter = `hue-rotate(${hue}deg)`;
+        };
+        clearInterval(thinkingInt);
         response.style.display = 'none';
         // response.innerText = message;
-        // setTimeout(() => {
-        //     response.style.display = 'block';
-        // }, 100);
+
+        if (lastMessage.includes('hey') || lastMessage.includes('heya') || lastMessage.includes('hello') || lastMessage.includes('hi')) {
+            response.innerText = questions.creativity[rando(questions.creativity.length - 1, 0)];
+        } else {
+            response.innerText = 'sorry can you please repeat that?';
+        };
+
+        setTimeout(() => {
+            response.style.display = 'block';
+        }, 100);
     }
 
     function sendMessage(message) {
@@ -39,8 +61,10 @@
             .replace(/[.*+?^${}()|[\]\\]/g, "\\$&")
             .trim();
             lastMessage = message;
+            thinkingInt = setInterval(thinking, 250);
             sendMessage(message);
             setTimeout(() => {
+                ta.placeholder = '';
                 ta.value = '';
                 ta.style.height = '20px';
             }, 30);
@@ -51,6 +75,16 @@
         if (window.innerWidth < 500) {
             send.style.right = `${(window.innerWidth * 0.1) - 3}px`;
         }
+    }
+
+    function thinking() {
+        console.log('think');
+        response.innerText = thinkingArr[thinkingCount];
+        thinkingCount += 1;
+        if (thinkingCount === 3) {
+            thinkingCount = 0;
+        };
+        console.log(thinkingCount);
     }
 
     function rando(max, min) {
@@ -65,8 +99,8 @@
     // socket
     // TODO: the socket will need to be changed when it is running of heroku
     // var socket = io.connect("http://localhost:8080");
-    // var socket = io.connect("http://192.168.1.158:8080");
-    var socket = io.connect("https://latentspace.herokuapp.com/");
+    var socket = io.connect("http://192.168.1.158:8080");
+    // var socket = io.connect("https://latentspace.herokuapp.com/");
 
     socket.on('hi_there', data => {
         const { message } = data;
